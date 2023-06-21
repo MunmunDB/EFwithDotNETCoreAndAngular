f4f322d1893406c822308b806457c37925423726
mdbhowmik <mdbhowmik@gmail.com>
2023-06-21 21:45:46 +0100

    Data access layer moved to folder

---
+++ DataAccessLayer/Model/CompanyDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Model
+{
+    [Table("Companies", Schema ="dbo")]
+    public class CompanyDB
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+        public  int Id { set; get; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        [Display(Name = "Company Name")]
+        public string CompanyName { set; get; }
+        public CompanyDB() { }
+        [Required]
+        [Column(TypeName = "nvarchar(10)")]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+public DateTime CreatedDate { set; get; }
+        public decimal SharePrice { get; set; }
+    }
+}
